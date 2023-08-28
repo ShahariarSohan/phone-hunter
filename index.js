c0d6bf350b439b54fb2822b2991405c93d2b7db8
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-28 10:04:18 +0600

    I have added phone card

---
+++ index.js
@@ -0,0 +1,27 @@
+const phoneLoad = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
+    const data = await res.json()
+    const phones = data.data
+    phoneCollection(phones)
+}
+phoneLoad()
+const phoneCollection = phones => {
+    const phoneContainer = document.getElementById('phone-container')
+    phones.forEach(phone => {
+        console.log(phone)
+        const phoneCard = document.createElement('div')
+        phoneCard.classList = "card  bg-base-100 shadow-xl p-5"
+        phoneCard.innerHTML = `        
+            <figure><img src="${phone.image}" alt="Iphone" /></figure>
+            <div class="card-body text-center">
+                <h2 class="text-2xl font-bold ">${phone.phone_name}</h2>
+                <p>There are many variations of passages of available, but the majority have suffered</p>
+                <h2 class="text-2xl font-bold">$900</h2>
+                <div class="card-actions justify-center">
+                <button class="btn btn-primary">Show Details</button>
+                </div>
+            </div>        
+            `
+        phoneContainer.appendChild(phoneCard)
+    });
+}
