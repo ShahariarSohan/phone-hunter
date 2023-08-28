1a1daa563bef2fe2f11f64a5bc5bcb7f9f3b7303
Shahariar Sohan <Sohanshahariar4@gmail.com>
2023-08-29 05:58:22 +0600

    Phone hunter website somehow completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //data receive,load and show
-const phoneLoad = async (searchText) => {
+const phoneLoad = async (searchText = 13) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`)
     const data = await res.json()
     const phones = data.data
@@ -19,7 +19,6 @@ const phoneCollection = phones => {
     const phoneContainer = document.getElementById('phone-container')
     phoneContainer.textContent = ''
     phones.forEach(phone => {
-        console.log(phone)
         const phoneCard = document.createElement('div')
         phoneCard.classList = "card  bg-base-100 shadow-xl p-5"
         phoneCard.innerHTML = `        
@@ -57,8 +56,28 @@ const loadingSpinner = (isLoading) => {
 }
 //show modal with dynamic data
 const showDetailsButton = async (id) => {
-    console.log(`Phone Model is :${id}`)
     const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
     const data = await res.json()
-    console.log(data)
+    const phone = data.data
+    console.log(phone)
+    showModalDetails(phone)
 }
+const showModalDetails = (phone) => {
+    const phoneDetailContainer = document.getElementById('phone-detail-container')
+    phoneDetailContainer.innerHTML = `
+    <img  class="mx-auto"src="${phone.image}"/>
+    <h1 id="title" class="text-2xl font-bold">${phone.name}</h1>
+    <p class="text-justify">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+     <h1 class=""><span class="text-xl font-bold">Storage:</span> ${phone?.mainFeatures?.storage} </h1>
+     <h1 class=""><span class="text-xl font-bold">DisplaySize:</span> ${phone?.mainFeatures?.displaySize} </h1>
+     <h1 class=""><span class="text-xl font-bold">Chipset:</span> ${phone?.mainFeatures?.chipSet} </h1>
+     <h1 class=""><span class="text-xl font-bold">Memory:</span> ${phone?.mainFeatures?.storage} </h1>
+     <h1 class=""><span class="text-xl font-bold">Slug:</span> ${phone?.slug} </h1>
+     <h1 class=""><span class="text-xl font-bold">ReleaseDate:</span> ${phone?.releaseDate} </h1>
+     <h1 class=""><span class="text-xl font-bold">Brand:</span> ${phone?.brand} </h1>
+     <h1 class=""><span class="text-xl font-bold">GPS:</span> ${phone?.others?.GPS} </h1>
+     
+     `
+    show_details.showModal()
+}
+phoneLoad()
